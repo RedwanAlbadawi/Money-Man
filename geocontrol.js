3cfe17f5894522321cf59d3ea2e58e383c62dcda
cannon-edgar <edgarwang95@gmail.com>
2015-07-14 19:34:43 -0400

    working google api calls
    got lat longs from google

---
+++ geocontrol.js
@@ -0,0 +1,53 @@
+(function() {
+    var map = L.map('map').setView([0, 0], 2),
+        geocoders = {
+            'Nominatim': L.Control.Geocoder.nominatim(),
+        },
+        selector = L.DomUtil.get('geocode-selector'),
+        control = new L.Control.Geocoder({ geocoder: null }),
+        btn,
+        selection,
+        marker;
+
+    function select(geocoder, el) {
+        if (selection) {
+            L.DomUtil.removeClass(selection, 'selected');
+        }
+
+        control.options.geocoder = geocoder;
+        L.DomUtil.addClass(el, 'selected');
+        selection = el;
+    }
+
+    for (var name in geocoders) {
+        btn = L.DomUtil.create('button', 'leaflet-bar', selector);
+        btn.innerHTML = name;
+        (function(n) {
+            L.DomEvent.addListener(btn, 'click', function() {
+                select(geocoders[n], this);
+            }, btn);
+        })(name);
+
+        if (!selection) {
+            select(geocoders[name], btn);
+        }
+    }
+
+    L.tileLayer('https://a.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=' + LCG.apiToken, {
+        attribution: '<a href="http://osm.org/copyright">Terms & Feedback</a>'
+    }).addTo(map);
+
+    control.addTo(map);
+
+    map.on('click', function(e) {
+        control.options.geocoder.reverse(e.latlng, map.options.crs.scale(map.getZoom()), function(results) {
+            var r = results[0];
+            if (r) {
+                if (marker) {
+                    map.removeLayer(marker);
+                }
+                marker = L.marker(r.center).bindPopup(r.html || r.name).addTo(map).openPopup();
+            }
+        })
+    });
+})();
